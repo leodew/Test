e0bdd52a5f553a56b95456338914783ce558043b
bnemer <bnemer@gmail.com>
2017-12-02 10:14:25 +0100

    test 1

---
+++ test.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      bnemer
+#
+# Created:     11/10/2014
+# Copyright:   (c) bnemer 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+def main():
+    for i in range(0,10):
+        print(i)
+
+
+if __name__ == '__main__':
+    # Open a file
+    main()
